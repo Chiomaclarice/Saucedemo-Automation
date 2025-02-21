972d45f1186fd038e7c516cb7fd0561ea799318b
Chiomaclarice <113688393+Chiomaclarice@users.noreply.github.com>
2025-02-20 22:38:27 -0600

    Login test scripts

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,42 @@
+
+import Login from "../pages/Login";
+const login = new Login()
+
+describe('Login Saucedemo', () => {
+    let loginData; //variable declaration
+
+    beforeEach(() => {
+    cy.fixture('loginData').then((data) => {
+        loginData = data; //storing fixture data in a variable
+        login.visit();
+    })
+    })
+
+  it('Login successfully on Saucedemo', ()=> {
+    login.getUsernameField().type(loginData.validCredentials.username);
+    login.getPasswordField().should('be.visible').type(loginData.validCredentials.password)
+    login.getLoginButton().click()
+
+    })
+   it('Verify unsuccessful login with invalid credentials', ()=>{
+    login.getUsernameField().type(loginData.invalidCredentials.username)
+    login.getPasswordField().type(loginData.invalidCredentials.password)
+    login.getLoginButton().click()
+    login.getErrorMessage(). should('contain','Username and password do not match any user in this service')
+})
+
+    it('Verify login fails for locked out user', ()=> {
+    login.getUsernameField().type(loginData.lockedUser.username)
+    login.getPasswordField().type(loginData.lockedUser.password)
+    login.getLoginButton().click()
+    login.getErrorMessage().should('contain.text', 'Sorry, this user has been locked out.')
+    })
+
+    it('Verify login fails when username and password fields are left empty', ()=> {
+    login.getLoginButton().click()
+    login.getErrorMessage(). should('contain', 'Username is required')
+
+    })
+
+
+})
---
+++ cypress/pages/Login.js
@@ -0,0 +1,24 @@
+
+
+class Login {
+    constructor() {
+    this.url = 'https://www.saucedemo.com/'
+    }
+     visit() {
+    cy.visit('/')
+     }
+
+     getUsernameField () {
+        return cy.get('#user-name').should('exist')
+     }
+     getPasswordField() {
+        return cy.get('#password');
+     }
+     getLoginButton () {
+        return cy.get('#login-button');
+     }
+     getErrorMessage() {
+        return cy.get('[data-test="error"]')
+     }
+}
+export default Login;
